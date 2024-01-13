e8ad1aace2cab2aa199b46a32e92be4637e8ea8d
minienglish1 <minienglish1@163.com>
2024-01-13 21:39:38 +0800

    inital

---
+++ download.py
@@ -0,0 +1,7 @@
+from huggingface_hub import snapshot_download
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--model", type=str, default="model", help="huggingface model name")
+args = parser.parse_args()
+
+snapshot_download(repo_id="args.model")
---
+++ download.sh
@@ -0,0 +1,2 @@
+source venv/bin/activate
+python instructBLIP_0.7-accelerate.py --img_dir /mnt/storage/caption/test --prompts /mnt/storage/caption/prompts.txt --model Salesforce/instructblip-flan-t5-xxl --overwrite
