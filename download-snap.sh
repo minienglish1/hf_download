139d6c5827bc9a84985604b3f9f31be3b7dbabef
minienglish1 <minienglish1@163.com>
2024-01-14 14:58:49 +0800

    #5

---
+++ download-snap.sh
@@ -0,0 +1,2 @@
+source venv/bin/activate
+python download.py --model Salesforce/instructblip-flan-t5-xl
--- download.py
+++ download.py
@@ -1,5 +1,6 @@
 import argparse
-from huggingface_hub import hf_hub_download
+#from huggingface_hub import hf_hub_download
+from huggingface_hub import snapshot_download
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--model", type=str, default="model", help="huggingface model name")
@@ -7,4 +8,5 @@ args = parser.parse_args()
 
 model = args.model
 print(model)
-hf_hub_download(repo_id=model)
+#hf_hub_download(repo_id=model)
+snapshot_download(repo_id=model)
