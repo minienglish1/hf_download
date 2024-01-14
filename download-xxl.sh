ec5ac9bdcc078ff180ad5666c08a694bf9894cd9
minienglish1 <minienglish1@163.com>
2024-01-14 08:12:59 +0800

    #4

---
+++ download-xxl.sh
@@ -0,0 +1,2 @@
+source venv/bin/activate
+huggingface-cli download Salesforce/instructblip-flan-t5-xxl
--- download.py
+++ download.py
@@ -1,5 +1,5 @@
 import argparse
-from huggingface_hub import snapshot_download
+from huggingface_hub import hf_hub_download
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--model", type=str, default="model", help="huggingface model name")
@@ -7,4 +7,4 @@ args = parser.parse_args()
 
 model = args.model
 print(model)
-snapshot_download(repo_id=model)
+hf_hub_download(repo_id=model)
